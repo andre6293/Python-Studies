838c45cbabf177466037b39b3703287072b24f8d
andre6293 <andrephilippe@live.com>
2020-05-26 20:50:23 -0300

    update 26/05/2020

---
+++ HackerRank/bonAppetit.py
@@ -0,0 +1,14 @@
+def bonAppetit (pratos, iAnnaNaoComeu, precos, cobrado):
+    amountOfPeople = 2
+    totalADividir = (sum(precos) - precos[iAnnaNaoComeu]) / amountOfPeople
+    if totalADividir < cobrado:
+        return cobrado - totalADividir
+    return "Bon Appetit"
+
+a = 4
+b = 1
+c = [3, 10, 2, 9]
+d = 6
+
+print(bonAppetit(a, b, c, d))
+
